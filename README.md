e24d9be0d00dd4b5213fc1f9ee0cfcdc266033ce
Uzi Cohen <uzicohen9@gmail.com>
2018-05-04 22:08:38 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Social Betting front end
+
+To run:
+Navigate to folder /social-betting-fe
+npm install
+npm run dev
+Navigate to localhost:8080
---
+++ src/js/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Alert } from 'react-bootstrap';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <Alert bsStyle="warning">
+        <strong>Something</strong>
+      </Alert>
+    );
+  }
+}
